fca9e353bf739f5dc617107ef8c07c69c0643dd6
Mah1uzAhmed <mahfuzahmed296@gmail.com>
2026-02-18 03:07:45 +0600

    day2

---
+++ Day2/basicsv2.py.py
@@ -0,0 +1,9 @@
+""" # ❤️lists❤️ """
+# thoughts =['this','can','hold','any','data','type','like','strings','numbers','booleans','lists','tuples','sets','dictionaries' ]
+# print(thoughts)
+# print(type(thoughts))
+
+import numpy as np
+item=np.array([1,2,3,4,5])
+print(item)
+print(type(item))
